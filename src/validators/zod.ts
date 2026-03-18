65c5831d856326dd10483e81e46aad5f40363fe1
Kristina Jeršova <kristinajershova5@gmail.com>
2026-03-18 13:14:51 +0200

    Added zod validation for book, review, publisher

--- src/validators/zod.ts
+++ src/validators/zod.ts
@@ -1,7 +1,34 @@
 import { z } from "zod"
 import { Book } from "../models/books.model"
 
-const Book = z.object({
+const books = z.object({
     id: z.number(),
 
 })
+
+export const bookSchema = z.object({
+  title: z.string(),
+  isbn: z.string(),
+  publishedYear: z.number(),
+  pageCount: z.number(),
+  language: z.string(),
+  description: z.string(),
+  authorId: z.string(),
+  publisherId: z.string(),
+  genres: z.array(z.string())
+});
+
+export const reviewSchema = z.object({
+  userName: z.string(),
+  rating: z.number().min(1).max(5),
+  comment: z.string()
+});
+
+export const publisherSchema = z.object({
+  id: z.number(),
+  bookId: z.number(),
+  userName: z.string(),
+  rating: z.number().min(1).max(5),
+  comment: z.string(),
+  createdAt: z.ZodDate,
+});
