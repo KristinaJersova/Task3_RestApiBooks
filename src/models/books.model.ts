4ab85f0b18619d239d3057de691f76d018f98d40
Kristina Jeršova <kristinajershova5@gmail.com>
2026-03-18 12:56:00 +0200

    Change book options, delete import genre, because didn't need there. And change type createdAt and updateAt, number to Date

--- src/models/books.model.ts
+++ src/models/books.model.ts
@@ -1,16 +1,15 @@
-import { Genre } from "./genre.model"
 export interface Book {
-    id: number;
-    title: string;
-    isbn: string;
-    publishedYear: number;
-    pageCount: number;
-    language: string;
-    description: string;
-    coverImage?: string | null | undefined;
-    authorId: number;
-    publisherId: number;
-    genres: string;
-    createdAt: number;
-    updatedAt: number;
+  id: number;
+  title: string;
+  isbn: string;
+  publishedYear: number;
+  pageCount: number;
+  language: string;
+  description: string;
+  coverImage?: string | null;
+  authorId: number;
+  publisherId: number;
+  genres: number[];
+  createdAt: Date;
+  updatedAt: Date;
 }
