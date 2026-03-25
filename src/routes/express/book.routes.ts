506476bf444ebe6d5281102eb14ef04f1a610260
Kristina Jeršova <kristinajershova5@gmail.com>
2026-03-25 10:55:02 +0200

    change commit error

--- src/routes/express/book.routes.ts
+++ src/routes/express/book.routes.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
-<<<<<<< HEAD:src/routes/express/book.routes.ts
-import * as bookController from "../../controllers/book.controller.js";
-=======
-import {createBookHandler,getBooksHandler,getBookByIdHandler,updateBookHandler,deleteBookHandler,} from "../../src/controllers/book.controller";
+import {createBookHandler,getBooksHandler,getBookByIdHandler,updateBookHandler,deleteBookHandler,} from "../../controllers/book.controller";
 
->>>>>>> 51bf02a21ee24d0149984f726bdefb4cfa81af6d:src/routes/book.routes.ts
 const router = Router();
 
 router.post("/api/v1/books", createBookHandler);
