08e201f55ab25d87dd6be36905228eab93263a27
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-09 17:10:49 +0300

    Change id to bookid

--- src/routes/express/book.routes.ts
+++ src/routes/express/book.routes.ts
@@ -4,13 +4,13 @@ import * as bookController from "../../controllers/book.controller";
 const router = Router();
 
 router.get("/books", bookController.getBooksHandler);
-router.get("/books/:id", bookController.getBookByIdHandler);
+router.get("/books/:bookId", bookController.getBookByIdHandler);
 router.post("/books", bookController.createBookHandler);
-router.put("/books/:id", bookController.updateBookHandler);
-router.delete("/books/:id", bookController.deleteBookHandler);
+router.put("/books/:bookId", bookController.updateBookHandler);
+router.delete("/books/:bookId", bookController.deleteBookHandler);
 
 router.post("/books/:bookId/reviews", bookController.createReviewHandler);
 router.get("/books/:bookId/reviews", bookController.getReviewsByBookHandler);
-router.get("/books/:id/average-rating", bookController.getAverageRatingHandler);
+router.get("/books/:bookId/average-rating", bookController.getAverageRatingHandler);
 
 export default router;
